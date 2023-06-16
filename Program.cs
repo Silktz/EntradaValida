7ce4d75a4a7df784e74e0a3be8049ce8c1f5dedb
Silktz <caua.franco3@etec.sp.gov.br>
2023-06-16 20:46:59 -0300

    pronto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//By Silktz
+
+int valor = -1;
+
+Console.WriteLine("--- Entrada Válida ---");
+
+Thread.Sleep(1500);
+
+while (valor < 0 || valor >9)
+{
+    Console.WriteLine("Digite um número entre 1 e 9 (ou 0 para cancelar)...:");
+    valor = Convert.ToInt32(Console.ReadLine()!.Trim());
+}
+if (valor >= 10)
+{
+Console.WriteLine("Valor Invalido, Digite um numero de 1 a 9 ");
+}
+else
+{
+if (valor == 0)
+{
+    Console.WriteLine("Operação Cancelada.");
+
+    Thread.Sleep(1500);
+
+    Console.WriteLine("Encerrando.....");
+    Environment.Exit(1);
+
+}
+else
+{
+    Console.WriteLine($"Número Selecionado: {valor} é valido");
+}}
